bafb1a55abf6c9cc1f1dc396db409aebc09e02c6
Jahid Anowar <milon5143@gmail.com>
2020-10-05 22:06:06 +0530

    Time fromat added in Post Card

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -45,34 +45,19 @@
             class="text-gray-600 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-300"
           >
             <svg
-              class="w-4 h-4 mr-1"
-              stroke="currentColor"
-              stroke-width="2"
+              class="w-6 h-6"
               fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              viewBox="0 0 24 24"
-            >
-              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-              <circle cx="12" cy="12" r="3"></circle></svg
-            >1.2K
-          </span>
-          <span
-            class="text-gray-600 inline-flex items-center leading-none text-sm"
-          >
-            <svg
-              class="w-4 h-4 mr-1"
               stroke="currentColor"
-              stroke-width="2"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
               viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
               ></path></svg
-            >{{ post.comments.length }}
+            >{{ post.updated_at | fromatDate }}
           </span>
         </div>
       </div>
