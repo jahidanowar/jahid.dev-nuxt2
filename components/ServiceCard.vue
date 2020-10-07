9dc86cf35936e1bee58ce13b7ab93d589f614b83
Jahid Adnowar <milon5143@gmail.com>
2020-10-07 14:28:44 +0530

    Service Card Improved

--- components/ServiceCard.vue
+++ components/ServiceCard.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="border border-gray-300 p-6 rounded-lg">
-    <div
-      class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-teal-100 text-primary mb-4"
-    >
+  <div class="p-6 rounded-lg shadow-lg border-t-2">
+    <div class="inline-flex items-center justify-center rounded-full mb-4">
       <slot></slot>
     </div>
     <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
