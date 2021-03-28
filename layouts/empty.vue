06f14ba72f1553ad6fd79ec1fd139d836a45ba2b
jahidanowar <milon5143@gmail.com>
2021-03-28 08:24:32 +0530

    Link Shortner Redirection added

---
+++ layouts/empty.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'emptyLayout'
+}
+</script>
+
+<style>
+html {
+  color: theme('colors.text');
+}
+</style>
---
+++ pages/go/_slug.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="link container mx-auto h-screen text-center flex items-center justify-center"
+  >
+    <div>
+      <lottie :width="300" :height="300" :options="lottieOptions" />
+      <h2 class="uppercase tracking-widest font-normal">Redirecting</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import lottie from 'vue-lottie/src/lottie.vue'
+import * as loadingAnimation from '~/assets/lottie/loading.json'
+export default {
+  layout: 'empty',
+  components: {
+    lottie
+  },
+  data() {
+    return {
+      lottieOptions: { animationData: loadingAnimation.default }
+    }
+  },
+  async asyncData({ $axios, params }) {
+    const links = await $axios.$get('/thirstylink/?slug=' + params.slug)
+    return { links }
+  },
+  head() {
+    return {
+      title: this.links[0].title.rendered,
+      meta: [{ hid: 'robots', name: 'robots', content: 'noindex' }]
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      window.location.replace(this.links[0]._ta_destination_url)
+    }, 3000)
+  }
+}
+</script>
