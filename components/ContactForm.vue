a7d004fae98ea70867d4869a09650cb2998b84fd
jahidanowar <milon5143@gmail.com>
2021-03-30 11:28:30 +0530

    Progress Indecator added

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -83,10 +83,33 @@
       placeholder="Description"
     ></textarea>
     <button
-      class="btn block w-full bg-primary hover:bg-secondary text-white"
+      class="btn w-full bg-primary hover:bg-secondary text-white inline-flex justify-center"
+      :disabled="loading"
       type="submit"
     >
-      Submit
+      <template v-if="!loading"> Submit </template>
+      <template v-else>
+        <svg
+          class="animate-spin ml-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </template>
     </button>
     <p
       v-if="message"
@@ -101,6 +124,7 @@
 export default {
   data() {
     return {
+      loading: false,
       contactForm: {
         fname: '',
         email: '',
@@ -120,15 +144,15 @@ export default {
   methods: {
     async onSubmit() {
       try {
-        const token = await this.$recaptcha.execute('login')
-        console.log('ReCaptcha token:', token)
+        this.loading = true
+
         // eslint-disable-next-line
         let formData = new FormData()
         for (const [key, value] of Object.entries(this.contactForm)) {
           // console.log(`${key}: ${value}`)
           formData.append(key, value)
         }
-        this.$axios
+        await this.$axios
           .post(
             'https://api.jahid.dev/wp-json/contact-form-7/v1/contact-forms/1191/feedback',
             formData
@@ -145,6 +169,8 @@ export default {
           })
       } catch (error) {
         console.log('Login error:', error)
+      } finally {
+        this.loading = false
       }
     }
   }
