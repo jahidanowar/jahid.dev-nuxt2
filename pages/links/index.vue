5c448a928f7fb90446bb02212fe4d9f684c502e4
jahidanowar <milon5143@gmail.com>
2021-04-19 18:32:51 +0530

    Links Page updated

--- pages/links/index.vue
+++ pages/links/index.vue
@@ -1,28 +1,31 @@
 <template>
-  <div
-    class="bio container mx-auto bg-gray-100 p-5 h-screen grid place-items-center"
-  >
-    <div class="w-full">
+  <div class="bg-gray-100 p-5 h-screen grid place-items-center">
+    <div class="bio container mx-auto sm:max-w-md">
       <header class="text-center">
         <h1 class="text-5xl">jahid</h1>
-        <p>entreprenure, programmer</p>
+        <p>entrepreneur, programmer</p>
       </header>
       <div class="links my-10">
         <nuxt-link
           to="/"
-          class="flex p-2 bg-white rounded-lg shadow justify-center mb-5"
-          >website</nuxt-link
+          class="flex p-2 bg-white rounded-lg shadow transform hover:-translate-y-2 justify-center mb-5"
+          >👉 website</nuxt-link
         >
         <nuxt-link
           to="/blog"
-          class="flex p-2 bg-white rounded-lg shadow justify-center mb-5"
-          >blog</nuxt-link
+          class="flex p-2 bg-white rounded-lg shadow transform hover:-translate-y-2 justify-center mb-5"
+          >🧑‍💻 blog</nuxt-link
         >
         <nuxt-link
           to="/contact"
-          class="flex p-2 bg-white rounded-lg shadow justify-center mb-5"
-          >contact</nuxt-link
-        >
+          class="flex p-2 bg-white rounded-lg shadow transform hover:-translate-y-2 justify-center mb-5"
+          >🤙 contact
+        </nuxt-link>
+        <a
+          href="https://foxflue.com"
+          class="flex p-2 bg-white rounded-lg shadow transform hover:-translate-y-2 justify-center mb-5"
+          >🤝 company
+        </a>
       </div>
       <div class="social mb-10">
         <social-buttons class="justify-center" />
