1cbc7729554ae41c8be6ae99867d691e7fd261cc
jahidanowar <milon5143@gmail.com>
2021-04-20 08:43:51 +0530

    Links Page Updated

--- components/SocialButtons.vue
+++ components/SocialButtons.vue
@@ -2,7 +2,7 @@
   <div class="flex">
     <a
       href="https://twitter.com/@jahidDev"
-      class="p-3 bg-gray-900 hover:bg-primary text-white rounded-full"
+      class="p-3 bg-gray-700 hover:bg-primary text-white rounded-full"
       ><svg
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
@@ -16,7 +16,7 @@
     <a
       href="https://www.linkedin.com/in/jahidanowar/"
       target="_blank"
-      class="p-3 bg-gray-900 hover:bg-primary text-white rounded-full ml-2"
+      class="p-3 bg-gray-700 hover:bg-primary text-white rounded-full ml-2"
       ><svg
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +29,7 @@
     ></a>
     <a
       href="https://facebook.com/jahid500k"
-      class="p-3 bg-gray-900 hover:bg-primary text-white rounded-full ml-2"
+      class="p-3 bg-gray-700 hover:bg-primary text-white rounded-full ml-2"
       ><svg
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
@@ -43,7 +43,7 @@
     ></a>
     <a
       href="https://www.youtube.com/channel/UCeLvns7Gwo_aq18-GKT-yww"
-      class="p-3 bg-gray-900 hover:bg-primary text-white rounded-full ml-2"
+      class="p-3 bg-gray-700 hover:bg-primary text-white rounded-full ml-2"
       ><svg
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
@@ -54,5 +54,6 @@
           d="M23.495 6.205a3.007 3.007 0 00-2.088-2.088c-1.87-.501-9.396-.501-9.396-.501s-7.507-.01-9.396.501A3.007 3.007 0 00.527 6.205a31.247 31.247 0 00-.522 5.805 31.247 31.247 0 00.522 5.783 3.007 3.007 0 002.088 2.088c1.868.502 9.396.502 9.396.502s7.506 0 9.396-.502a3.007 3.007 0 002.088-2.088 31.247 31.247 0 00.5-5.783 31.247 31.247 0 00-.5-5.805zM9.609 15.601V8.408l6.264 3.602z"
         /></svg
     ></a>
+    <slot></slot>
   </div>
 </template>
---
+++ components/SocialIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <a
+    :href="link"
+    class="p-3 bg-gray-700 hover:bg-primary text-white rounded-full ml-2"
+    ><slot></slot
+  ></a>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      default: '#',
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
